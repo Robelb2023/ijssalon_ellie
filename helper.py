be645f80ee4f8e88599c715b065f5d6383ca5df2
Rob <rwse@live.nl>
2024-05-25 17:04:29 +0200

    functies toegevoegd

--- helper.py
+++ helper.py
@@ -9,10 +9,7 @@ def decoreer(tekst=""):
 def fooi_pp(bedrag, personen):
     bedrag_pp = bedrag/personen
     return f"Het bedrag per persoon is {bedrag_pp} euro"
-    
-   
-
-
+ 
 def onderstreep(tekst=""):
     uit = []
     uit.append(tekst)
